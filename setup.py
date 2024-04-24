51271d8e6ff2db7b790c00fdccc2f0ab9eb98697
Kip Parker <kip@kipparker.co.uk>
2024-04-24 17:31:45 +0100

    Hard code version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
-from ckanext.spatial import __version__
+# from ckanext.spatial import __version__
 
-version = __version__
+version = "2.1.1"
 
 setup(
     name="ckanext-spatial",
